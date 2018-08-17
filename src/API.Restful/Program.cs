4a94f2e2a8bee2db4799f784470ba796aac215a3
Rasmus Olsson <olsson.rasmus90@gmail.com>
2018-08-17 14:01:43 +0200

    removed init pop

--- src/API.Restful/Program.cs
+++ src/API.Restful/Program.cs
@@ -2,7 +2,6 @@
 
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace API.Restful
 {
@@ -11,13 +10,6 @@ namespace API.Restful
         public static void Main(string[] args)
         {
             var host = BuildWebHost(args);
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                var context = services.GetService<DataContext>();
-                DataSeeder.SeedCountries(context);
-            }
-
             host.Run();
         }
 
